eb2eb177079ff17411311e1115a8ad6960047f2c
Pasindu <passbudd@gmail.com>
2025-10-18 10:05:39 +0530

    Created Day9

--- Day8/src/Methodes.java
+++ Day8/src/Methodes.java
@@ -9,6 +9,17 @@ public class Methodes {
         //oddOrEven();
         System.out.print("Enter a number : ");;
         oddOrEven2(scan.nextInt());
+
+        getSum("Pasindu" , 23,45,234);
+
+        //for each loop
+
+        int[] ar2 = {1,2,3,4,5,6};
+
+        for(int data : ar2){
+            System.out.println(data);
+            System.out.println(data);
+        }
     }
 
     public static void printHello(){
@@ -36,5 +47,28 @@ public class Methodes {
         }else{
             System.out.println(number +" is a odd number.");
         }
+    }
+
+    //variable arguments ---> varargs
+    public static void getSum(String name, int... data){
+
+    }
+
+    // method overloading
+
+    public static void createAccount(){
+        // By changing parameter count
+    }
+
+    public static void createAccount(int x){
+        //By changing parameter type
+    }
+
+    public static void createAccount(String x){
+
+    }
+
+    public static void createAccount(String x, int[] num){
+
     }
 }
---
+++ Day9/src/Student.java
@@ -0,0 +1,9 @@
+
+// original common data sheet
+public class Student {
+    String name;
+    int age;
+    String address;
+    boolean isSmoke;
+    double salary;
+}
